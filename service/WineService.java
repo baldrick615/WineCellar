b21ccfebdb6783434b15fe13be581dfc521e050e
Tim McGrath <tmcgrath28@gmail.com>
2021-09-09 16:56:59 -0500

    Add files via upload

--- service/WineService.java
+++ service/WineService.java
@@ -2,9 +2,15 @@ package com.trm.winecellar.service;
 
 import java.util.List;
 
+import com.trm.winecellar.dao.WineCellarImpl;
 import com.trm.winecellar.dao.WineDAO;
 import com.trm.winecellar.dao.WineDAOMock;
+import com.trm.winecellar.model.RequestError;
+import com.trm.winecellar.model.Varietal;
 import com.trm.winecellar.model.Wine;
+
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
 
 public class WineService {
 	private WineDAO wineDAO = new WineDAOMock();
@@ -14,7 +20,23 @@ public class WineService {
 	}
 	
 	public List<Wine> getWinesByVintage(Integer vintage){
+		validateVintageYear(vintage);
 		return wineDAO.getWinesByVintage(vintage);
+	}
+	
+	private void validateVintageYear(Integer vintage) {
+		if (vintage < 1950 || vintage > 2035) {
+			RequestError err = new RequestError(1, "The vintage is outside nnormal range (1950 - 2035). Please check your input.");
+			Response response = Response.status(400)
+					.entity(err)
+					.build();
+			throw new WebApplicationException(response);
+		}
+		
+	}
+
+	public List<Wine> getWinesByVarietal(Varietal varietal){
+		return wineDAO.getWinesByVarietal(varietal);
 	}
 
 }
