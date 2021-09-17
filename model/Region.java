7ef7fcd1e75f74bfbf30c75adce9985d2ad3f88b
Tim McGrath <tmcgrath28@gmail.com>
2021-09-17 08:46:52 -0500

    Add files via upload

--- model/Region.java
+++ model/Region.java
@@ -1,15 +1,16 @@
 package com.trm.winecellar.model;
 
 public enum Region {
-	au, ca, wa, usOther, chile, argentina, france, italy, spain, euOther, ROW;
+	au, ca, wa, usOther, chile, argentina, france, italy, spain, nz, euOther, ROW;
 
-	public static Region convertStringToVarietal(String value) {
+	public static Region convertStringToRegion(String value) {
 		Region convertedRegion = null;
 		for (Region region : Region.values()) {
-			if (convertedRegion.toString().equalsIgnoreCase(value)) {
+			if (region.toString().equalsIgnoreCase(value)) {
 				convertedRegion = region;
 			}
 		}
 		return convertedRegion;
 	}
+
 }
