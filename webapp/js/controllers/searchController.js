289373c23ff32c62297aca752f26bcf06d2459cc
Tim McGrath <tmcgrath28@gmail.com>
2021-09-26 14:40:55 -0500

    adding files

--- webapp/js/controllers/searchController.js
+++ webapp/js/controllers/searchController.js
@@ -1,4 +1,4 @@
-app.controller('searchController', function ($scope, $http) {
+app.controller('searchController', function ($scope, $http, $location) {
   $scope.getAllWines = function () {
     $http.get('/winecellar/webapi/wines').then(function (response) {
       $scope.wines = response.data;
--- webapp/js/controllers/updateController.js
+++ webapp/js/controllers/updateController.js
@@ -1,14 +1,33 @@
-app.controller('updateController', function ($scope, $routeParams, $location, $http) {
-  // Select wine by name, then edit any available field
-  $http.get("/winecellar/webapi/wines/" + $routeParams.id)
-  .then(function(response){
-    let wines = response.data;
-    if(wines.length == 1) {
-      $scope.wine = wine[0]
-    } else {
-      .error (function(err){{
-        return err;
-      }
-    }
-  })
-});
+app.controller(
+  'updateController',
+  function ($scope, $routeParams, $location, $http) {
+    // Select wine by name, then edit any available field
+    $http.get('/winecellar/webapi/wines/' + $routeParams.id).then(
+      function (response) {
+        let wines = response.data;
+        if (wines.length == 1) {
+          $scope.wine = wine[0];
+        } else {
+          //.error (function(err){{
+          //return err;
+        }
+      },
+      ($scope.goToSearchView = function () {
+        console.log('go to search view');
+        $location.path('/search');
+      }),
+      ($scope.updateWine = function () {
+        $http.put('winecellar/webapi/wines', $scope.wine).then(
+          function (response) {
+            $scope.updateStatus = 'update successful';
+          },
+          function (response) {
+            $scope.updateStatus = 'error updating information';
+          }
+        );
+      }),
+
+      $scope.getWinesById()
+    
+  }
+);
